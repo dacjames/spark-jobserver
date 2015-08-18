3e5f7758fc0a7737f086eba22823858b30c662b6
Ankit Soni <ankit.soni@applovin.com>
2015-08-18 16:35:51 -0700

    add dev/prod settings

---
+++ bin/settings.dev.sh
@@ -0,0 +1,6 @@
+LOG_DIR=/var/log/spark-jobserver
+SPARK_HOME=/usr/lib/spark
+SPARK_CONF_HOME=/usr/lib/spark/conf
+DRIVER_MEMORY=10g
+appdir=/usr/lib/spark-jobserver
+PIDFILE=$appdir/spark-jobserver.pid
