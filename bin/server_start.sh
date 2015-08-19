79e6b2fb12e0234eb16c59e4ade8d80638de6291
Ankit Soni <ankit.soni@applovin.com>
2015-08-19 14:56:36 -0700

    wait on jobserver in start script

--- bin/server_start.sh
+++ bin/server_start.sh
@@ -38,5 +38,4 @@ fi
 $SPARK_HOME/bin/spark-submit --class $MAIN --driver-memory $DRIVER_MEMORY \
     --conf "spark.executor.extraJavaOptions=$LOGGING_OPTS" \
     --driver-java-options "$GC_OPTS $JAVA_OPTS $LOGGING_OPTS $CONFIG_OVERRIDES" \
-    $@ $appdir/spark-job-server.jar $conffile &>> $LOG_DIR/jobserver.log &
-echo $! > $PIDFILE
+    $@ $appdir/spark-job-server.jar $conffile &>> $LOG_DIR/jobserver.log
